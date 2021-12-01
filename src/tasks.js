03a0fc57e7285886a59c4590014c1cc3b4d9d2f5
cyb1ove <cyb1ove.ua@gmail.com>
2021-12-01 17:59:57 +0200

    Task was solved

--- src/tasks.js
+++ src/tasks.js
@@ -1,89 +1,165 @@
-// const jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-// const { window } = new JSDOM(`...`);
-
 function duplicate(arr) {
-    return [...arr, ...arr];
+  return [...arr, ...arr];
 }
 
-function getFoo() {
-    return 10 + '20';
+function concateNumberToString() {
+  return 10 + "20";
 }
 
 function countFloat() {
-    return 0.1 + 0.2 == 0.3;
+  return 0.1 + 0.2 == 0.3;
 }
 
 function getLength() {
-    var foo = [];
-    foo.push(1);
-    foo.push(2);
+  var foo = [];
+  foo.push(1);
+  foo.push(2);
 
-    return foo.length;
+  return foo.length;
 }
 
 function getFooX() {
-    var foo = {n: 1};
-    var bar = foo;
-    foo.x = foo = {n: 2};
+  var foo = { n: 1 };
+  var bar = foo;
+  foo.x = foo = { n: 2 };
 
-    return foo.x;
+  return foo.x;
 }
 
 function callStackPrint() {
-    console.log('one');
+  console.log("one");
 
-    Promise.resolve().then(function() {
-        console.log('three');
-    })
+  Promise.resolve().then(function () {
+    console.log("three");
+  });
 
-    setTimeout(function() {
-        console.log('two');
-    }, 0);
+  setTimeout(function () {
+    console.log("two");
+  }, 0);
 
-    console.log('four');
+  console.log("four");
 }
 
-function IIFE() {
-    (function(){
-        var a = b = 3;
-    })();
-      
-    console.log("a defined? " + (typeof a !== 'undefined') + "\n");
-    console.log("b defined? " + (typeof b !== 'undefined'));
+function callVarFromIIFE() {
+  (function () {
+    var a = b = 3;
+  })();
+
+  console.log("a defined? " + (typeof a !== "undefined"));
+  console.log("b defined? " + (typeof b !== "undefined"));
 }
 
-function operands() {
-    console.log("hello" || "world");
-    console.log("foo" && "bar");
+function getOperands() {
+  console.log("hello" || "world");
+  console.log("foo" && "bar");
 }
 
 function add(x, y = undefined) {
-    if (!y) {
-        return (z) => x + z;
-    }
+  if (!y) {
+    return z => x + z;
+  }
 
-    return x + y;
+  return x + y;
 }
 
 function reverseString(str) {
-    return str.split("").reverse().join("");
+  return str.split("").reverse().join("");
 }
 
 function windowFoo() {
-    return ( window.foo || ( window.foo = "bar" ) );
+  return {}.foo || ({}.foo = "bar");
+}
+
+function fizzbizzloop() {
+  let i;
+
+  for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(`${i} fizzbuzz `);
+    }
+
+    if (i % 3 === 0) {
+      console.log(`${i} fizz `);
+    }
+
+    if (i % 5 === 0) {
+      console.log(`${i} buzz `);
+    }
+  }
+}
+
+function foo1() {
+  return {
+    bar: "hello",
+  };
+}
+
+function foo2() {
+  return;
+  {
+    bar: "hello";
+  }
+}
+
+function doSomething() {
+  return Promise.resolve();
+}
+
+function doSomethingElse() {
+  return 4;
+}
+
+function promise1() {
+  return doSomething().then(function () {
+    return doSomethingElse();
+  });
+}
+
+function promise2() {
+  return doSomething().then(function () {
+    doSomethingElse();
+  });
+}
+
+function promise3() {
+  return doSomething().then(doSomethingElse());
+}
+
+function promise4() {
+  return doSomething().then(doSomethingElse);
+}
+
+function callTwoAlerts() {
+  try {
+    var foo = "Hello";
+    (function () {
+      var bar = " World";
+      global.alert(foo + bar);
+    })();
+    global.alert(foo + bar);
+  } catch {
+    throw new Error("Bar is not find");
+  }
 }
 
 module.exports = {
-    duplicate,
-    getFoo,
-    countFloat,
-    getLength,
-    getFooX,
-    callStackPrint,
-    IIFE,
-    operands,
-    add,
-    reverseString,
-    windowFoo
-}
+  duplicate,
+  concateNumberToString,
+  countFloat,
+  getLength,
+  getFooX,
+  callStackPrint,
+  callVarFromIIFE,
+  getOperands,
+  add,
+  reverseString,
+  fizzbizzloop,
+  foo1,
+  foo2,
+  promise1,
+  promise2,
+  promise3,
+  promise4,
+  windowFoo,
+  callTwoAlerts
+};
--- src/tasks.test.js
+++ src/tasks.test.js
@@ -2,88 +2,150 @@
 
 const {
     duplicate,
-    getFoo,
+    concateNumberToString,
     countFloat,
     getLength,
     getFooX,
     callStackPrint,
-    IIFE,
-    operands,
+    callVarFromIIFE,
+    getOperands,
     add,
     reverseString,
-    windowFoo
+    fizzbizzloop,
+    foo1,
+    foo2,
+    promise1,
+    promise2,
+    promise3,
+    promise4,
+    windowFoo,
+    callTwoAlerts
 } = require('./tasks');
 
 
-test('duplicate array', () => {
+test('Array must be duplicated', () => {
     const arr = [1, 2, 3];
 
     expect(duplicate(arr))
         .toStrictEqual([1, 2, 3, 1, 2, 3]);
 })
 
-test('get foo', () => {
-    expect(getFoo())
+test('Function must return string', () => {
+    expect(concateNumberToString())
         .toStrictEqual('1020')
 })
 
-test('count float', () => {
+test('Float numbers count incorrect', () => {
     expect(countFloat())
         .toStrictEqual(false);
 })
 
-test('get length', () => {
+test('Array length must equal 2', () => {
     expect(getLength())
         .toStrictEqual(2);
 })
 
-test('get foo.x', () => {
+test('Object property refer to unexistanse property from another object', () => {
     expect(getFooX())
         .toStrictEqual(undefined)
 })
 
-test('test callstack print', async () => {
-    let outputData = "";
-    const storeLog = inputs => (outputData += inputs);
+test('Functions must be called accorging event loop', async () => {
+    let outputData = [];
+    const verifiable = ['one', 'four', 'three', 'two'];
+    const storeLog = inputs => (outputData.push(inputs));
 
     jest.useFakeTimers();
     console.log = jest.fn(storeLog)
+
     await callStackPrint();
+    
     jest.advanceTimersByTime(0);
 
-    expect(outputData).toBe('onefourthreetwo');
+    expect(outputData.join('\n')).toBe(verifiable.join('\n'));
     jest.useRealTimers();
 })
 
-test('IIFE', () => {
-    let outputData = "";
-    const storeLog = inputs => (outputData += inputs);
+test('Initialized variables without key word create its in global scope', () => {
+    let outputData = [];
+    const verifiable = ["a defined? false", "b defined? true"]
+    const storeLog = inputs => (outputData.push(inputs));
 
     console.log = jest.fn(storeLog);
-    IIFE();
-    expect(outputData).toBe(
-        "a defined? false\n" +
-        "b defined? true"
-    );
+    callVarFromIIFE();
+    expect(outputData.join('\n')).toBe(verifiable.join('\n'));
 });
 
-test('get operands from logical expression', () => {
+test('Logical expressions must return first or last operand', () => {
     let outputData = "";
     const storeLog = inputs => (outputData += inputs);
 
     console.log = jest.fn(storeLog);
-    operands();
+    getOperands();
 
     expect(outputData).toBe("hellobar");
 })
 
-test('reverse string', () => {
+test('String must be reversed', () => {
     expect(reverseString("i'm a lasagna hog"))
         .toStrictEqual("goh angasal a m'i");
 })
 
-test('test add with carrying', () => {
+test('Function must be carrying', () => {
     expect(add(5, 2)).toStrictEqual(7);
     expect(add(5)(2)).toStrictEqual(7);
 })
 
+test('Loop must work correctly', () => {
+    const verifiable = "3 fizz 5 buzz 6 fizz 9 fizz 10 buzz "
+        + "12 fizz 15 fizzbuzz 15 fizz 15 buzz 18 fizz "
+        + "20 buzz 21 fizz 24 fizz 25 buzz 27 fizz 30 fizzbuzz "
+        + "30 fizz 30 buzz 33 fizz 35 buzz 36 fizz 39 fizz 40 buzz "
+        + "42 fizz 45 fizzbuzz 45 fizz 45 buzz 48 fizz 50 buzz "
+        + "51 fizz 54 fizz 55 buzz 57 fizz 60 fizzbuzz 60 fizz "
+        + "60 buzz 63 fizz 65 buzz 66 fizz 69 fizz 70 buzz 72 fizz "
+        + "75 fizzbuzz 75 fizz 75 buzz 78 fizz 80 buzz 81 fizz 84 fizz "
+        + "85 buzz 87 fizz 90 fizzbuzz 90 fizz 90 buzz 93 fizz 95 buzz 96 fizz 99 fizz 100 buzz "
+
+    let outputData = "";
+    const storeLog = inputs => (outputData += inputs);
+
+    console.log = jest.fn(storeLog);
+    fizzbizzloop();
+
+    expect(outputData).toBe(verifiable);
+})
+
+test('Function must return undefined if return statement breaks immediately', () => {
+    expect(foo1()).toEqual({ bar: "hello"});
+    expect(foo2()).toBe(undefined);
+})
+
+test('Promises must work correctly', async () => {
+    const resultOfPromise1 = await promise1();
+    const resultOfPromise2 = await promise2();
+    const resultOfPromise3 = await promise3();
+    const resultOfPromise4 = await promise4();
+
+    expect(resultOfPromise1).toBe(4);
+    expect(resultOfPromise2).toBe(undefined);
+    expect(resultOfPromise3).toBe(undefined);
+    expect(resultOfPromise4).toBe(4);
+})
+
+test('OR logical expression must return last operand', () => {
+    expect(windowFoo()).toBe("bar");
+})
+
+test('Alert must return error if it call variable from another scope', () => {
+    let alertMessage = "";
+    const storeMessage = input => (alertMessage += input);
+
+    try {
+        global.alert = jest.fn(storeMessage);
+        callTwoAlerts();
+    } catch (error) {
+        expect(error.message).toBe("Bar is not find");
+        expect(alertMessage).toBe("Hello World");
+    }
+})
